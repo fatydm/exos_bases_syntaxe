e71f682dc95b53b96f3285b07d560ce336fe836a
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-01-29 14:04:07 +0100

    exercice2

---
+++ exercice2.js
@@ -0,0 +1,24 @@
+// 1) En javascript, déclare 2 variables dans lesquelles tu affecteras respectivement
+// ton nom et prénom (choisis un nom pertinent pour chaque variable !). 
+// Avec l’instruction alert(……), tu afficheras "Bonjour, nom prénom".
+
+const nom = "Diarra"
+const prenom = "Faty"
+
+// alert("Bonjour, " + nom + " " + prenom)
+console.log("Bonjour, " + nom + " " + prenom)
+
+// 2) En javascript, déclare une variable constante et assigne lui une valeur numérique, 
+// c'est-à-dire un nombre, par exemple, 64.
+
+const number = 64
+
+// Déclare une autre variable qui contient ton âge.
+const age = 30
+
+// Déclare une autre variable qui contiendra le résultat de la soustraction de 64 moins ton âge en utilisant les deux variables précédentes.
+const result = number - age
+
+// Avec l’instruction alert(….) , tu afficheras "Résultat soustraction années à travailler avant la retraite ! 
+// Courage ! En développant ce sera plus fun ! ".
+console.log(result + " années à travailler avant la retraite ! Courage ! En développant ce sera plus fun ! ")
